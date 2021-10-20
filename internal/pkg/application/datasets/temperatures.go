612d2bec09c325291b1a033d0fb709667cb619b8
Isak Styf <isak.styf@masarinconsulting.se>
2021-10-20 12:37:53 +0200

    Added stubs for get temps and value struct

--- internal/pkg/application/datasets/temperatures.go
+++ internal/pkg/application/datasets/temperatures.go
@@ -1,6 +1,7 @@
 package datasets
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/diwise/api-opendata/internal/pkg/infrastructure/logging"
@@ -9,8 +10,22 @@ import (
 func NewRetrieveTemperaturesHandler(log logging.Logger, contextBrokerURL string) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		temps, _ := getSomeTemperatures()
+		for _, t := range temps {
+			fmt.Printf("temp: %f\n", t.Value)
+		}
+
 		w.Header().Add("Content-Type", "application/json")
 		w.Write([]byte{})
 
 	})
+}
+
+// TODO: Refaktorisera och flytta till domänlagret
+type Temp struct {
+	Value float64
+}
+
+func getSomeTemperatures() ([]Temp, error) {
+	return nil, fmt.Errorf("not implemented")
 }
