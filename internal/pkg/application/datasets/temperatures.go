0768f46bf3bae71fffdc81d6e4025edaa03e02d9
Isak Styf <isak.styf@masarinconsulting.se>
2021-10-21 10:06:31 +0200

    Decreased default temp time window to last 24 hours

--- internal/pkg/application/datasets/temperatures.go
+++ internal/pkg/application/datasets/temperatures.go
@@ -31,7 +31,7 @@ func NewRetrieveTemperaturesHandler(log logging.Logger, svc TempService) http.Ha
 
 		response := &TempResponse{}
 
-		temps, _ := svc.Get(time.Now().UTC().Add(-7*24*time.Hour), time.Now().UTC())
+		temps, _ := svc.Get(time.Now().UTC().Add(-1*24*time.Hour), time.Now().UTC())
 
 		tempRespItemMap := make(map[string]TempResponseItem)
 
@@ -86,40 +86,39 @@ type TempService interface {
 	Get(from, to time.Time) ([]Temp, error)
 }
 
+func NewTempService(contextBrokerURL string) TempService {
+	return &ts{contextBrokerURL: contextBrokerURL}
+}
+
 type ts struct {
 	contextBrokerURL string
 }
 
 func (svc ts) Get(from, to time.Time) ([]Temp, error) {
-	return getSomeTemperatures(svc.contextBrokerURL, from, to)
-}
-
-func NewTempService(contextBrokerURL string) TempService {
-	return &ts{contextBrokerURL: contextBrokerURL}
-}
-
-func getSomeTemperatures(contextBrokerURL string, from, to time.Time) ([]Temp, error) {
-	var err error
 
 	timeAt := from.Format(time.RFC3339)
 	endTimeAt := to.Format(time.RFC3339)
-	url := fmt.Sprintf("%s/ngsi-ld/v1/entities?type=WeatherObserved&attrs=temperature&georel=near;maxDistance==2000&geometry=Point&coordinates=[17.3051555,62.3908926]&timerel=between&timeAt=%s&endTimeAt=%s", contextBrokerURL, timeAt, endTimeAt)
+
+	url := fmt.Sprintf(
+		"%s/ngsi-ld/v1/entities?type=WeatherObserved&attrs=temperature&georel=near%%3BmaxDistance==2000&geometry=Point&coordinates=[17.3051555,62.3908926]&timerel=between&timeAt=%s&endTimeAt=%s",
+		svc.contextBrokerURL, timeAt, endTimeAt,
+	)
 
 	response, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request failed, status code not ok: %s", err)
-	}
 	defer response.Body.Close()
 
-	temps := []Temp{}
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request failed, status code not ok: %d", response.StatusCode)
+	}
+
 	wos := []fiware.WeatherObserved{}
-
 	b, _ := io.ReadAll(response.Body)
-
 	err = json.Unmarshal(b, &wos)
+
+	temps := []Temp{}
 
 	for _, wo := range wos {
 		t := Temp{
