0e7c4c21f809b6496aef52fadd689b05d06b13e9
Isak Styf <isak.styf@masarinconsulting.se>
2021-11-17 22:31:31 +0100

    Set default paths to ease local container start

--- cmd/api-opendata/main.go
+++ cmd/api-opendata/main.go
@@ -38,8 +38,8 @@ func main() {
 	log := logging.NewLogger()
 	log.Infof("Starting up %s ...", serviceName)
 
-	flag.StringVar(&openApiSpecFileName, "oas", "", "An OpenAPI specification to be served on /api/openapi")
-	flag.StringVar(&datasetFileName, "rdffile", "", "The file to serve datasets from")
+	flag.StringVar(&openApiSpecFileName, "oas", "/opt/diwise/openapi.json", "An OpenAPI specification to be served on /api/openapi")
+	flag.StringVar(&datasetFileName, "rdffile", "/opt/diwise/datasets/dcat.rdf", "The file to serve datasets from")
 	flag.Parse()
 
 	datafile := openDatasetsFile(log, datasetFileName)
