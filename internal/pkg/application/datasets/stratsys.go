e8d47663ea4345c8a1bc491c662932c865943822
shestyf <sofia.styf@masarinconsulting.se>
2021-11-26 10:54:41 +0100

    checking in parameters in handler

--- internal/pkg/application/datasets/stratsys.go
+++ internal/pkg/application/datasets/stratsys.go
@@ -8,6 +8,15 @@ import (
 
 func NewRetrieveStratsysReportsHandler(log logging.Logger, companyCode, clientID, scope string) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if companyCode == "" || clientID == "" || scope == "" {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Error("all environment variables need to be set")
+			return
+		}
+
+		// http.Post with clientID and scope to get token
+
+		// use token and company code to get reports
 
 	})
 }
