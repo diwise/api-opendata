678aa35231603f7d4fc3783d042c58a68590d9f7
Isak Styf <isak.styf@masarinconsulting.se>
2022-06-15 11:46:59 +0200

    fix syntax error after log package change

--- internal/pkg/presentation/handlers/temperatures.go
+++ internal/pkg/presentation/handlers/temperatures.go
@@ -73,7 +73,7 @@ func NewRetrieveTemperaturesHandler(logger zerolog.Logger, svc services.TempServ
 		sensor := r.URL.Query().Get("sensor")
 		if sensor == "" {
 			w.WriteHeader(http.StatusBadRequest)
-			log.Error("no sensor specified in temperature request")
+			log.Error().Msg("no sensor specified in temperature request")
 			return
 		}
 
