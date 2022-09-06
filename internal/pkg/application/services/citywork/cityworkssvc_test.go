86a563212686ff2e13b93c748cb6d5c82e753db1
shestyf <sofia.styf@masarinconsulting.se>
2022-09-06 13:52:31 +0200

    created small test for cityworks. may need better tests written

--- internal/pkg/application/services/citywork/cityworkssvc_test.go
+++ internal/pkg/application/services/citywork/cityworkssvc_test.go
@@ -1,13 +1,26 @@
 package citywork
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/matryer/is"
+	"github.com/rs/zerolog"
 )
 
-func TestXxx(t *testing.T) {
+func TestThatGetByIDReturnsErrorOnCityworkNotFound(t *testing.T) {
+	is := is.New(t)
+	server := setupMockServiceThatReturns(200, "")
+	log := zerolog.Logger{}
 
+	cityworksSvc := NewCityworksService(context.Background(), log, server.URL, "default")
+	defer cityworksSvc.Shutdown()
+
+	cityworkBytes, err := cityworksSvc.GetByID("cityworkID")
+	is.True(err != nil)
+	is.Equal(cityworkBytes, []byte{})
 }
 
 func setupMockServiceThatReturns(responseCode int, body string) *httptest.Server {
