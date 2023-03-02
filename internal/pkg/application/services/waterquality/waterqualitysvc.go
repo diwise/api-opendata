068a7b565558c7f8c3ae1eee4aae90f52f0ff28e
shestyf <sofia.styf@masarinconsulting.se>
2023-03-02 02:31:45 +0100

    adding waterquality svc and creating initial tests

--- internal/pkg/application/services/waterquality/waterqualitysvc.go
+++ internal/pkg/application/services/waterquality/waterqualitysvc.go
@@ -2,9 +2,16 @@ package waterquality
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"sync"
+	"time"
 
+	"github.com/diwise/api-opendata/internal/pkg/domain"
 	"github.com/rs/zerolog"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 type WaterQuality interface {
@@ -17,28 +24,40 @@ type WaterQuality interface {
 	GetAll() []byte
 }
 
-func NewWaterQualityService(ctx context.Context, log zerolog.Logger, url, tenant string, maxDistance int) WaterQuality {
+type WaterQualityQuery interface {
+	BetweenTimes(from, to time.Time) WaterQualityQuery
+	Point(latitude, longitude float64) WaterQualityQuery
+}
+
+func NewWaterQualityService(ctx context.Context, log zerolog.Logger, url, tenant string) WaterQuality {
 	return &wqsvc{
 		contextBrokerURL: url,
 		tenant:           tenant,
 
 		waterQualities:      []byte("[]"),
 		waterQualityDetails: map[string][]byte{},
-		maxDistance:         maxDistance,
 
 		ctx: ctx,
 		log: log,
 	}
 }
 
+type wqq struct {
+	from      time.Time
+	to        time.Time
+	latitude  float64
+	longitude float64
+	distance  int
+}
+
 type wqsvc struct {
+	wqq
 	contextBrokerURL string
 	tenant           string
 
 	wqoMutex            sync.Mutex
 	waterQualities      []byte
 	waterQualityDetails map[string][]byte
-	maxDistance         int
 
 	ctx context.Context
 	log zerolog.Logger
@@ -71,6 +90,166 @@ func (svc *wqsvc) GetAll() []byte {
 	return svc.waterQualities
 }
 
-func (svc *wqsvc) run() {
+func (svc *wqsvc) GetByID(id string) ([]byte, error) {
+	svc.wqoMutex.Lock()
+	defer svc.wqoMutex.Unlock()
 
+	body, ok := svc.waterQualityDetails[id]
+	if !ok {
+		return []byte{}, fmt.Errorf("no such water quality observed")
+	}
+
+	return body, nil
+}
+
+func (svc *wqsvc) GetByLocation(latitude, longitude float64) ([]byte, error) {
+
+	//ask for suggestions on how best to do this
+
+	return nil, nil
+}
+
+func (svc *wqsvc) run() {
+	nextRefreshTime := time.Now()
+
+	for svc.keepRunning {
+		if time.Now().After(nextRefreshTime) {
+			svc.log.Info().Msg("refreshing water quality info")
+			err := svc.refresh()
+			if err != nil {
+				svc.log.Error().Err(err).Msg("failed to refresh water quality info")
+				// Retry every 10 seconds on error
+				nextRefreshTime = time.Now().Add(10 * time.Second)
+			} else {
+				svc.log.Info().Msgf("refreshed water qualities")
+				// Refresh every 5 minutes of success
+				nextRefreshTime = time.Now().Add(5 * time.Minute)
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+	svc.log.Info().Msg("water quality service exiting")
+}
+
+func (svc *wqsvc) refresh() error {
+	// /temporal/entities/
+	// within range of request
+	// default time last 24 hrs
+
+	// ask which water quality observations exist first to build list of sensors, then for each sensor the historical/temporal data
+
+	wqoBytes, err := svc.requestData(svc.ctx, svc.log, svc.contextBrokerURL)
+	if err != nil {
+		svc.log.Error().Err(err).Msg("failed to retrieve water quality data")
+		return nil
+	}
+
+	wqos := []WaterQualityTemporal{}
+	err = json.Unmarshal(wqoBytes, &wqos)
+	if err != nil {
+		svc.log.Error().Err(err).Msg("failed to unmarshal water qualities")
+		return err
+	}
+
+	for _, wq := range wqos {
+		details := WaterQualityTemporal{
+			ID:          wq.ID,
+			Location:    wq.Location,
+			Temperature: wq.Temperature,
+		}
+
+		jsonBytes, err := json.Marshal(details)
+		if err != nil {
+			svc.log.Error().Err(err).Msg("failed to marshal water quality to json")
+			return err
+		}
+
+		svc.storeWaterQualityDetails(wq.ID, jsonBytes)
+	}
+
+	jsonBytes, err := json.Marshal(wqos)
+	if err != nil {
+		svc.log.Error().Err(err).Msg("failed to marshal water qualities to json")
+		return err
+	}
+
+	svc.storeWaterQualityList(jsonBytes)
+
+	return nil
+}
+
+func (svc *wqsvc) storeWaterQualityDetails(id string, body []byte) {
+	svc.wqoMutex.Lock()
+	defer svc.wqoMutex.Unlock()
+
+	svc.waterQualityDetails[id] = body
+}
+
+func (svc *wqsvc) storeWaterQualityList(body []byte) {
+	svc.wqoMutex.Lock()
+	defer svc.wqoMutex.Unlock()
+
+	svc.waterQualities = body
+}
+
+func (q *wqq) requestData(ctx context.Context, log zerolog.Logger, ctxBrokerURL string) ([]byte, error) {
+	var err error
+
+	httpClient := http.Client{
+		Transport: otelhttp.NewTransport(http.DefaultTransport),
+	}
+
+	url := fmt.Sprintf(
+		"%s/ngsi-ld/v1/temporal/entities?type=WaterQualityObserved",
+		ctxBrokerURL,
+	)
+
+	if !q.from.IsZero() && !q.to.IsZero() {
+		url = url + fmt.Sprintf("&timerel=between&time=%s&endTime=%s", q.from.Format(time.RFC3339), q.to.Format(time.RFC3339))
+	} else {
+		q.from = time.Now().UTC().Add(-24 * time.Hour)
+		q.to = time.Now().UTC()
+		url = url + fmt.Sprintf("&timerel=between&time=%s&endTime=%s", q.from.Format(time.RFC3339), q.to.Format(time.RFC3339))
+	}
+
+	if q.latitude > 0.0 && q.longitude > 0.0 {
+		url = fmt.Sprintf("%s&georel=near%%3BmaxDistance==%d&geometry=Point&coordinates=[%f,%f]", url, q.distance, q.latitude, q.longitude)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create http request: %w", err)
+	}
+
+	response, err := httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("request failed: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request failed, status code not ok: %d", response.StatusCode)
+	}
+
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %s", err)
+	}
+
+	return b, nil
+}
+
+type WaterQualityTemporal struct {
+	ID       string `json:"id"`
+	Location struct {
+		Type  string       `json:"type"`
+		Value domain.Point `json:"value"`
+	} `json:"location"`
+	Temperature []Value `json:"temperature"`
+}
+
+type Value struct {
+	Value      float64 `json:"value"`
+	ObservedAt string  `json:"observedAt"`
 }
--- internal/pkg/application/services/waterquality/waterqualitysvc_test.go
+++ internal/pkg/application/services/waterquality/waterqualitysvc_test.go
@@ -1,1 +1,116 @@
 package waterquality
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	testutils "github.com/diwise/service-chassis/pkg/test/http"
+	"github.com/diwise/service-chassis/pkg/test/http/expects"
+	"github.com/diwise/service-chassis/pkg/test/http/response"
+	"github.com/matryer/is"
+	"github.com/rs/zerolog"
+)
+
+func TestWaterQualityRuns(t *testing.T) {
+	is, log, svc := testSetup(t, http.StatusOK, "[]")
+	wq := NewWaterQualityService(context.Background(), log, svc.URL(), "default")
+
+	svcMock := wq.(*wqsvc)
+
+	err := svcMock.refresh()
+
+	is.NoErr(err)
+}
+
+func TestGetAll(t *testing.T) {
+	is, log, svc := testSetup(t, http.StatusOK, waterqualityJson)
+	wq := NewWaterQualityService(context.Background(), log, svc.URL(), "default")
+
+	svcMock := wq.(*wqsvc)
+
+	err := svcMock.refresh()
+
+	is.NoErr(err)
+
+	wqos := svcMock.GetAll()
+	is.True(wqos != nil)
+
+	expectation := `[{"id":"urn:ngsi-ld:WaterQualityObserved:temperature:se:servanet:lora:sk-elt-temp-02:2021-05-18T19:23:09Z","location":{"type":"GeoProperty","value":{"type":"Point","coordinates":[17.39364,62.297684]}},"temperature":[{"value":10.8,"observedAt":"2021-05-18T19:23:09Z"}]}]`
+
+	is.Equal(string(wqos), expectation)
+}
+
+func TestGetByID(t *testing.T) {
+	is, log, svc := testSetup(t, http.StatusOK, waterqualityJson)
+	wq := NewWaterQualityService(context.Background(), log, svc.URL(), "default")
+
+	svcMock := wq.(*wqsvc)
+
+	err := svcMock.refresh()
+
+	is.NoErr(err)
+
+	wqo, err := svcMock.GetByID("urn:ngsi-ld:WaterQualityObserved:temperature:se:servanet:lora:sk-elt-temp-02:2021-05-18T19:23:09Z")
+	is.NoErr(err)
+	is.True(wqo != nil)
+
+	expectation := `{"id":"urn:ngsi-ld:WaterQualityObserved:temperature:se:servanet:lora:sk-elt-temp-02:2021-05-18T19:23:09Z","location":{"type":"GeoProperty","value":{"type":"Point","coordinates":[17.39364,62.297684]}},"temperature":[{"value":10.8,"observedAt":"2021-05-18T19:23:09Z"}]}`
+
+	is.Equal(string(wqo), expectation)
+}
+
+var Expects = testutils.Expects
+var Returns = testutils.Returns
+var anyInput = expects.AnyInput
+
+func testSetup(t *testing.T, statusCode int, responseBody string) (*is.I, zerolog.Logger, testutils.MockService) {
+	is := is.New(t)
+	log := zerolog.Logger{}
+
+	ms := testutils.NewMockServiceThat(
+		Expects(is, anyInput()),
+		Returns(
+			response.Code(statusCode),
+			response.ContentType("application/ld+json"),
+			response.Body([]byte(responseBody)),
+		),
+	)
+
+	return is, log, ms
+}
+
+const waterqualityJson string = `[{
+	"@context": [
+	  "https://schema.lab.fiware.org/ld/context",
+	  "https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld"
+	],
+	"dateObserved": {
+	  "type": "Property",
+	  "value": {
+		"@type": "DateTime",
+		"@value": "2021-05-18T19:23:09Z"
+	  }
+	},
+	"id": "urn:ngsi-ld:WaterQualityObserved:temperature:se:servanet:lora:sk-elt-temp-02:2021-05-18T19:23:09Z",
+	"location": {
+	  "type": "GeoProperty",
+	  "value": {
+		"coordinates": [
+		  17.39364,
+		  62.297684
+		],
+		"type": "Point"
+	  }
+	},
+	"refDevice": {
+	  "object": "urn:ngsi-ld:Device:temperature:se:servanet:lora:sk-elt-temp-02",
+	  "type": "Relationship"
+	},
+	"temperature": [{
+	  "type": "Property",
+	  "value": 10.8,
+	  "observedAt": "2021-05-18T19:23:09Z"
+	}],
+	"type": "WaterQualityObserved"
+  }]`
